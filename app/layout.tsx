58378c24bcb5a91c4740f05368b691bee8b7351b
Kraiovyi Denys <tonopik@gmail.com>
2026-03-01 16:43:35 +0200

    add dark mode theme toggler

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import NavBar from '@/components/NavBar'
+import { ThemeProvider } from '@/components/theme/theme-provider'
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
@@ -23,11 +25,20 @@ export default function RootLayout({
 	children: React.ReactNode
 }>) {
 	return (
-		<html lang='en'>
+		<html lang='en' suppressHydrationWarning>
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				{children}
+				{' '}
+				<ThemeProvider
+					attribute='class'
+					defaultTheme='system'
+					enableSystem
+					disableTransitionOnChange
+				>
+					<NavBar />
+					{children}
+				</ThemeProvider>
 			</body>
 		</html>
 	)
---
+++ components/NavBar.tsx
@@ -0,0 +1,11 @@
+import { ModeToggle } from './theme/ThemeButtonToggle'
+
+const NavBar = () => {
+	return (
+		<div className='dark:bg-background'>
+			<ModeToggle />
+		</div>
+	)
+}
+
+export default NavBar
